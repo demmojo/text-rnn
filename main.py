f2cbf263310f1f4cb8efdd0b30700eafac06b1f8
demmojo <mo.hassaniv@gmail.com>
2018-07-01 16:47:55 +0300

    Changed default training dataset

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from rnn import CharGen
 from rnn.train_model import train
 
-train_new_model = True
+train_new_model = False
+model_name = 'shakespeare'
 
 if train_new_model:
-    textgen = CharGen(name="shakespeare",
+    textgen = CharGen(name=model_name,
                       bidirectional=True,
                       rnn_size=128,
                       rnn_layers=3,
                       embedding_dims=75,  # Size of the embedding layer (default 75)
                       input_length=60  # Number of characters considered for prediction (default 60)
                       )
-    train(text_filepath='datasets/test.txt',
+    train(text_filepath='datasets/shakespeare.txt',
           textgen=textgen,
           num_epochs=10,
           batch_size=512,
@@ -19,8 +20,8 @@ if train_new_model:
 
     print(textgen.model.summary())
 else:
-    textgen = CharGen(weights_filepath='models/test_weights.hdf5',
-                      vocab_filepath='models/test_vocabulary.json',
-                      config_filepath='models/test_config.json')
+    textgen = CharGen(weights_filepath='models/{}_weights.hdf5'.format(model_name),
+                      vocab_filepath='models/{}_vocabulary.json'.format(model_name),
+                      config_filepath='models/{}_config.json'.format(model_name))
 
-    train('datasets/test.txt', textgen, train_new_model=train_new_model, num_epochs=10)
+    train('datasets/shakespeare.txt', textgen, train_new_model=train_new_model, num_epochs=10)
