75cd3ca912c17b1fa150cec3fa07be87ef0e5388
demmojo <mo.hassaniv@gmail.com>
2018-07-02 00:16:08 +0300

    Added documentation about sigmoid recurrent activation choice

--- rnn/model.py
+++ rnn/model.py
@@ -1,8 +1,9 @@
 from keras.optimizers import RMSprop
 from keras.layers import Embedding, Dense, LSTM, Bidirectional
 from keras.engine.input_layer import Input
-from keras.layers import concatenate, SpatialDropout1D
+from keras.layers import concatenate, CuDNNLSTM, SpatialDropout1D
 from keras.models import Model
+from keras import backend as K
 from .AttentionLayer import WeightedAttentionAverage
 
 
@@ -41,19 +42,37 @@ def chargen_model(num_of_classes, cfg, weights_filepath=None, dropout=0.05, opti
     return model
 
 
-def new_rnn_layer(cfg, layer_num):
+def new_rnn_layer(cfg, num_layer):
     """Creates new RNN layer for each parameter depending on whether it is bidirectional LSTM or not.
+       Uses the fast LSTM implementation backed by CuDNN if a GPU is available.
+       Note: The normal LSTMs utilize sigmoid recurrent activations so as to retain compatibility CuDNNLSTM:
+       see the following github issue for more details: https://github.com/keras-team/keras/issues/8860
+
     :param cfg: configuration of CharGen instance
-    :param layer_num: ordinal number of the rnn layer being built
+    :param num_layer: ordinal number of the rnn layer being built
     :return: 3D tensor if return sequence is True
     """
-    if not cfg['bidirectional']:
+
+    gpu_no = len(K.tensorflow_backend._get_available_gpus())
+    if gpu_no > 0:
+        print('GPU is available...')
+        if cfg['bidirectional']:
+            return Bidirectional(CuDNNLSTM(cfg['rnn_size'],
+                                           return_sequences=True),
+                                 name='rnn_{}'.format(num_layer))
+
+        return CuDNNLSTM(cfg['rnn_size'],
+                         return_sequences=True,
+                         name='rnn_{}'.format(num_layer))
+    else:
+        print('No GPU available...')
+        if cfg['bidirectional']:
+            return Bidirectional(LSTM(cfg['rnn_size'],
+                                      return_sequences=True,
+                                      recurrent_activation='sigmoid'),
+                                 name='rnn_{}'.format(num_layer))
+
         return LSTM(cfg['rnn_size'],
                     return_sequences=True,
                     recurrent_activation='sigmoid',
-                    name='rnn_{}'.format(layer_num))
-
-    return Bidirectional(LSTM(cfg['rnn_size'],
-                              return_sequences=True,
-                              recurrent_activation='sigmoid'),
-                         name='rnn_{}'.format(layer_num))
+                    name='rnn_{}'.format(num_layer))
